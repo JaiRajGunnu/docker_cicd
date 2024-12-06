3339db7353140b4475e04698b2a207f70eca243a
JaiRajGunnu <jairajgsklm@gmail.com>
2024-12-06 12:11:10 +0530

    Read operations

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ app.use(bodyParser.json());
 let data = [];
 
 // Create - Add a new item
+
 app.post("/items", (req, res) => {
     const { id, name } = req.body;
 
@@ -19,5 +20,24 @@ app.post("/items", (req, res) => {
     data.push({ id, name });
     res.status(201).json({ message: "Item added successfully!", data });
 });
+
+
+// Read - Get all items
+app.get("/items", (req, res) => {
+    res.json(data);
+});
+
+// Read - Get item by ID
+app.get("/items/:id", (req, res) => {
+    const { id } = req.params;
+    const item = data.find((d) => d.id === id);
+
+    if (!item) {
+        return res.status(404).json({ message: "Item not found!" });
+    }
+
+    res.json(item);
+});
+
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
